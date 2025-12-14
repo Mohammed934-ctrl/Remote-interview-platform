a0af2084b86cee6e1088893bc16ee73ede40e139
practice12344 <godltxspark@gmail.com>
2025-12-14 16:49:38 +0530

    chnage in file are commited

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,15 +1,30 @@
 import express from "express";
+import path from "path"
 const app = express();
 
 import { ENV } from "../lib/env.js";
-import {  connectionDB } from "../lib/DB.js";
+import { connectionDB } from "../lib/DB.js";
+
+const __dirname = path.resolve();
 
 // console.log(ENV.PORT)
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
   res.status(200).json({
     msg: "hello from server file",
   });
 });
+app.get("/game", (req, res) => {
+  res.status(200).json({
+    msg: "game is ready",
+  });
+});
+
+if ((ENV.NODE_ENV = "production")) {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+  });
+}
 
 const startserver = async () => {
   try {
@@ -22,5 +37,4 @@ const startserver = async () => {
   }
 };
 
-
-startserver()
+startserver();
