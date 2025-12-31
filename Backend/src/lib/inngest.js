499df52ffe4e97c9a6bc89169795104aef35a80a
practice12344 <godltxspark@gmail.com>
2026-01-01 02:26:00 +0530

    changes in inngest.js file

--- Backend/src/lib/inngest.js
+++ Backend/src/lib/inngest.js
@@ -6,35 +6,28 @@ import { Createandupdatestreamuser, deletestreamuser } from "./Stream.js";
 export const inngest = new Inngest({ id: "Remote-interview" });
 
 const syncuser = inngest.createFunction(
-  {
-    id: "sync-user",
-  },
+  
+  { id: "sync-user" },
   { event: "clerk/user.created" },
-
   async ({ event }) => {
-    try {
-      await connectionDB();
+    await connectionDB();
 
-      const { id, email_addresses, first_name, last_name, image_url } =
-        event.data;
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
-      const newUser = {
-        clerkId: id,
-        email: email_addresses[0]?.email_address,
-        name: `${first_name || ""} ${last_name || ""}  `,
-        profileImage: image_url,
-      };
-      await User.create(newUser);
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`,
+      profileImage: image_url,
+    };
 
-      await upsertStreamUser({
-        id: newUser.clerkId.toString(),
-        name: newUser.name,
-        image: newUser.profileImage,
-      });
-    } catch (error) {
-      console.error("SYNC USER FAILED:", error);
-      throw error;
-    }
+    await User.create(newUser);
+
+    await Createandupdatestreamuser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   }
 );
 
